dfa4428bcd1705527d85c4b76d0983aa1757dd91
jjteoh <thewebdev.jjteoh@example.com>
2025-04-09 19:04:10 +0800

    build: setup vercel analytics
    setup vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
+
 
 export const metadata: Metadata = {
   title: "Kanban Board",
@@ -17,6 +20,9 @@ export default function RootLayout({
       <body className="flex flex-col h-screen overflow-hidden">
         {children}
         <Toaster />
+
+        <SpeedInsights />
+        <Analytics />
       </body>
 
     </html>
